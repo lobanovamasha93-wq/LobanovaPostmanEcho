5c2b2d98ca94c318996fb4a7883244761c31a039
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-04-02 15:58:04 +0400

    ci: add AppVeyor config, tests failed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -26,6 +26,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет, мир!"));
+                .body(, equalTo("Привет, мир!"));
     }
 }
